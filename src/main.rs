b21beadbef891f73c4d81983691367e9c89f5fb7
ArunHarish <arunhari97@yahoo.com>
2025-01-11 22:41:12 +1000

    Fix format

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
-use std::io::{self, BufWriter, Write};
 use std::fmt;
+use std::io::{self, BufWriter, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::time::Duration;
 
@@ -10,17 +10,17 @@ use dbus::blocking::{Connection, Proxy};
 use dbus::{arg, Message};
 
 // Threads and communication
-use std::thread;
 use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
 
 // To check whether socket exists
 use std::fs::{exists, remove_file};
 
 // For libc signal handling
-use libc::{sigaction, SIGTERM, SIGINT, SA_SIGINFO};
+use libc::{sigaction, SA_SIGINFO, SIGINT, SIGTERM};
 use std::mem;
-use std::ptr;
 use std::process::exit;
+use std::ptr;
 
 const SOCKET_PATH: &str = "/tmp/theme-listener.sock";
 
@@ -103,7 +103,9 @@ fn detect_freedesktop_theme() -> Result<Theme, dbus::Error> {
     Ok(theme)
 }
 
-fn listen_freedesktop_theme(condvar_pair: Arc<(Mutex<Theme>, Condvar)>) -> Result<bool, dbus::Error> {
+fn listen_freedesktop_theme(
+    condvar_pair: Arc<(Mutex<Theme>, Condvar)>,
+) -> Result<bool, dbus::Error> {
     let connection = Connection::new_session()?;
     let proxy = connection.with_proxy(
         "org.freedesktop.portal.Desktop",
@@ -111,16 +113,18 @@ fn listen_freedesktop_theme(condvar_pair: Arc<(Mutex<Theme>, Condvar)>) -> Resul
         Duration::from_millis(5000),
     );
 
-    let _ = proxy.match_signal(move |h: OrgFreeDesktopPortalDesktop, _: &Connection, _: &Message| {
-        if h.sender == "org.freedesktop.appearance" && h.key == "color-scheme" {
-            let (mutex, condvar) = &*condvar_pair;
-            let mut current_theme_value = mutex.lock().unwrap();
-            let next_theme_value = h.value.as_i64().unwrap();
-            *current_theme_value = to_theme(next_theme_value);
-            condvar.notify_all();
-        }
-        true
-    });
+    let _ = proxy.match_signal(
+        move |h: OrgFreeDesktopPortalDesktop, _: &Connection, _: &Message| {
+            if h.sender == "org.freedesktop.appearance" && h.key == "color-scheme" {
+                let (mutex, condvar) = &*condvar_pair;
+                let mut current_theme_value = mutex.lock().unwrap();
+                let next_theme_value = h.value.as_i64().unwrap();
+                *current_theme_value = to_theme(next_theme_value);
+                condvar.notify_all();
+            }
+            true
+        },
+    );
 
     loop {
         connection.process(Duration::from_millis(1000))?;
@@ -142,21 +146,21 @@ fn handle_connect(socket_stream: UnixStream, condvar_pair: Arc<(Mutex<Theme>, Co
     let (mutex, condvar) = &*condvar_pair;
     let mut theme_value = mutex.lock().unwrap();
     loop {
-       theme_value = condvar.wait(theme_value).unwrap();
-       // On error stop block listen to theme value
-       if let Err(_) = write_to_stream(&mut stream, theme_value.to_string().as_bytes()) {
-           break;
+        theme_value = condvar.wait(theme_value).unwrap();
+        // On error stop block listen to theme value
+        if let Err(_) = write_to_stream(&mut stream, theme_value.to_string().as_bytes()) {
+            break;
         }
     }
 }
 
 extern "C" fn handle_terminate() {
     // Delete the socket file
-   match remove_file(SOCKET_PATH) {
-       Ok(_) => {},
-       Err(_) => println!("Error removing socket file"),
-   }
-   exit(0);
+    match remove_file(SOCKET_PATH) {
+        Ok(_) => {}
+        Err(_) => println!("Error removing socket file"),
+    }
+    exit(0);
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -182,7 +186,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let theme_condvar_main_pair = Arc::new((Mutex::new(Theme::DARK), Condvar::new()));
     let theme_condvar_publisher_pair = Arc::clone(&theme_condvar_main_pair);
 
-    thread::spawn(move ||  listen_freedesktop_theme(theme_condvar_publisher_pair));
+    thread::spawn(move || listen_freedesktop_theme(theme_condvar_publisher_pair));
 
     for stream in listener.incoming() {
         match stream {
